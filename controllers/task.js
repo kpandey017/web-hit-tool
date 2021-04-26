0c7c92f4b1ac8072341b95b82810369f29b6af29
kpandey017 <kpandey017@gmail.com>
2021-04-26 12:12:59 +0530

    Update task.js

--- controllers/task.js
+++ controllers/task.js
@@ -150,7 +150,7 @@ exports.navigateUrl=  (tasks)=> {
     }
     (async () => {
         const browser = await puppeteer.launch({ headless: false ,
-     args: ['--disable-setuid-sandbox', '--no-sandbox']})
+     args: []})
         const page = await browser.newPage()
         await page.setViewport({ width: 1280, height: 1800 });
 
