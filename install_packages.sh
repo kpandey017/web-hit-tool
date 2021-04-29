d2c24cbbe01a6c52184d8a91b2a86a65830ab75f
kpandey017 <kpandey017@gmail.com>
2021-04-29 18:54:41 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,1 @@
-git clone https://github.com/kpandey017/web-hit-tool.git
-cd web-hit-tool
-npm install
-node index.js
+xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &
