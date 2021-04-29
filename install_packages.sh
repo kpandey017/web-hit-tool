36e5388253a6226573e02d5a1f13b0caa76ac7a7
kpandey017 <kpandey017@gmail.com>
2021-04-29 15:42:38 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,10 +1,4 @@
-curl -fsSL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo dpkg --install google-chrome-stable_current_amd64.deb
 git clone https://github.com/kpandey017/web-hit-tool.git
 cd web-hit-tool
 npm install
+node index.js
