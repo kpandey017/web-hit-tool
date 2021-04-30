b708d84d16f7a47bb9d8f46352797f40de3a18a9
kpandey017 <kpandey017@gmail.com>
2021-04-30 20:29:26 +0530

    Update execute_script.js

--- execute_script.js
+++ execute_script.js
@@ -1,11 +1,8 @@
-const { exec } = require('child_process');
-exec("xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &", (err, stdout, stderr) => {
-  if (err) {
-    //some err occurred
-    console.error(err)
-  } else {
-   // the *entire* stdout and stderr (buffered)
-   console.log(`stdout: ${stdout}`);
-   console.log(`stderr: ${stderr}`);
-  }
-});
+
+const { execSync } = require('child_process');
+// stderr is sent to stdout of parent process
+// you can set options.stdio if you want it to go elsewhere
+
+//const stdout = execSync('Xvfb -ac :99 -screen 0 1280x1024x16 &');
+const stdout1 = execSync('export DISPLAY=:99');
+const stdout2 = execSync('node /home/notadmin/web-hit-tool/index.js');
