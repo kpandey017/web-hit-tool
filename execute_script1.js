c3580268939b41eb8d7382f6850fd4d6c6e198d4
kpandey017 <kpandey017@gmail.com>
2021-04-30 15:36:41 +0530

    Create execute_script1.js

---
+++ execute_script1.js
@@ -0,0 +1,8 @@
+const { execSync } = require('child_process');
+// stderr is sent to stdout of parent process
+// you can set options.stdio if you want it to go elsewhere
+const { spawnSync} = require('child_process');
+const child = spawnSync("xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &", );
+console.error('error', child.error);
+console.log('stdout ', child.stdout);
+console.error('stderr ', child.stderr);
