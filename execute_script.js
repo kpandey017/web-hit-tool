e9861d7c5a25f1f51f3379aca9e4d5cc2a8b1fd4
Ubuntu <notadmin@vm1262857.ja5waezg2z5urj3hvlmwug2mwf.rx.internal.cloudapp.net>
2021-05-01 12:44:10 +0000

    from vm

--- execute_script.js
+++ execute_script.js
@@ -1,11 +1,20 @@
-const { exec } = require('child_process');
-exec("xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &", (err, stdout, stderr) => {
-  if (err) {
-    //some err occurred
-    console.error(err)
-  } else {
-   // the *entire* stdout and stderr (buffered)
-   console.log(`stdout: ${stdout}`);
-   console.log(`stderr: ${stderr}`);
-  }
-});
+const { execSync } = require('child_process');
+// stderr is sent to stdout of parent process
+// you can set options.stdio if you want it to go elsewhere
+
+try{
+console.log("step 1");
+ const stdout = execSync('Xvfb -ac :99 -screen 0 1280x1024x16');
+}
+catch (ex){
+
+console.log("step 1 - error");
+
+}
+
+//console.log("step 2");
+
+//const stdout1 = execSync('export DISPLAY=:99');
+
+console.log("step 3");
+const stdout2 = execSync('node /home/notadmin/web-hit-tool/index.js');
--- execute_script1.js
+++ execute_script1.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 // stderr is sent to stdout of parent process
 // you can set options.stdio if you want it to go elsewhere
 const { spawnSync} = require('child_process');
-const child = spawnSync("xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &", );
+const child = spawnSync(["xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &"] );
 console.error('error', child.error);
 console.log('stdout ', child.stdout);
 console.error('stderr ', child.stderr);
