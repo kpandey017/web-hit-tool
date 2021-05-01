efc7621fd6c45e769a9a764983f1afe6bab0841c
kpandey017 <kpandey017@gmail.com>
2021-05-01 16:52:41 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,1 +1,3 @@
-xvfb-run -a --server-args=' - screen 0 1280x800x24 - ac - nolisten tcp - dpi 96 + extension RANDR' node /home/notadmin/web-hit-tool/index.js &
+Xvfb -ac :99 -screen 0 1280x1024x16 &
+export DISPLAY=:99
+node /home/notadmin/web-hit-tool/index.js
