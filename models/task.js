7c0290dd1408a7dd65d3d8bfecbc834bc5854171
kpandey017 <kpandey017@gmail.com>
2021-05-14 22:41:03 +0530

    Update task.js
    Updated task model

--- models/task.js
+++ models/task.js
@@ -3,8 +3,13 @@ var Schema = mongoose.Schema;
 
 var TasksSchema = new Schema({
     url: {type: String, required: true},
+    uid: {type: String, required: true},
+    region:[{type: String}],
     startDate: {type: Date, required: false},
     lastDate: {type: Date, required: false},
+    todayVisit: {type: Number, required: false},
+    totalVisit: {type: Number, required: false},
+    lastVisit: {type: Date, required: false},
 });
 
 
